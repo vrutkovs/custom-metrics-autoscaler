ed445a6b6d8bc02ed4b67fae622dfa2301b38a48
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-08-08 11:09:09 +0200

    Update load script

--- load.sh
+++ load.sh
@@ -1,1 +1,1 @@
-podman run --rm -ti ecliptik/docker-siege -d 1 -c ${1:-1} https://app-static-site.apps.vrutkovs.devcluster.openshift.com/docs/docs/index.html
+podman run --rm -ti ecliptik/docker-siege -d 1 -c ${1:-1} https://app-static-site.apps.vrutkovs.devcluster.openshift.com/
